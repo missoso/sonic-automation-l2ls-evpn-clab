a7f2404856c8ec12ed757b13cb4ddb6b1ee49243
Miguel <43352409+missoso@users.noreply.github.com>
2026-02-16 16:19:30 +0000

    Update config file name in deployment script

--- deploy_sonic_baseline_cfg.sh
+++ deploy_sonic_baseline_cfg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp configs/leaf1-config_baseline.json admin@172.80.80.11:/home/admin/
 ssh admin@172.80.80.11 << EOF 
-sudo mv leaf1-config.json /etc/sonic/config_db.json
+sudo mv leaf1-config_baseline.json /etc/sonic/config_db.json
 sudo config reload -y
 EOF
